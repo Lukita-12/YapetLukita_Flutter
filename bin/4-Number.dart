f39d37ebfbad68a86719bcaad148e4acf1cde3a3
Lukita-12 <lukitayapet@gmail.com>
2023-09-29 22:22:36 +0700

    4-Number desuwa~

---
+++ bin/4-Number.dart
@@ -0,0 +1,22 @@
+void main() {
+// Number
+  // Terdapat dua jenis tipe data Number, int(Bulat) dan double(Desimal).
+  // Penulisan koma dalam double menggunakan titik, bukan koma.
+
+  int number1 = 0;
+  double number2 = 12.12;
+
+  print(number1);
+  print(number2);
+
+// num
+  // Tipe data number yang bisa int ataupun double, kita bisa menggunakan tipe data num.
+
+  // Bilanan bulat
+  num number3 = 10;
+  print(number3);
+
+  // Bilangan desimal
+  num number4 = 10.5;
+  print(number4);
+}
