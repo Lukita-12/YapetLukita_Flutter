89f0cb1d35a60a692893ec86460482c3b70c9bdb
Lukita-12 <lukitayapet@gmail.com>
2023-09-29 20:09:21 +0700

    HelloWorld-01 desu~

---
+++ bin/Program_HelloWorld.dart
@@ -0,0 +1,6 @@
+void main() {
+  print('Hello World');
+  // Print, menampilkan tulisan di Dart
+  // print memiliki parameter data string, menggunakan kutip
+  // Untuk menjalankan dart = dart run bin/Program_HelloWorld.dart
+}
--- bin/array.dart
+++ bin/array.dart
@@ -1,4 +1,4 @@
 void main() {
   var nama = ["Rafi", "Grave", "Yapet", "Ai-Chan"];
-  var umur = [30, 25, 20, 21];
+  var umur = [30, 25, 20, 20];
 }
