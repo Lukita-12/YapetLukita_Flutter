2158a2c859ca266275083d520fc3b7cfe4d1138c
Lukita-12 <lukitayapet@gmail.com>
2023-09-29 22:07:06 +0700

    3-Komentar~

---
+++ bin/3-Komentar.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Komentar digunakan untuk menambahkan dokumentasi pada kode program.
+  // Single-line = // ...
+  // Multi-line /* ... */
+  // Documentation, komentar yang digunakan sebagai  dart documentation.
+
+  var name = "Yapet Lukita";
+  // Single-line
+
+  /*
+  Multi-line
+  Multi-line
+  Multi-line
+  */
+
+  /// Documentation
+  print(name);
+}
