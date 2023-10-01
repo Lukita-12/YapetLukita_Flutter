56ad6f0fceca3c1d215f071f458f71b3845ffcb6
Lukita-12 <lukitayapet@gmail.com>
2023-10-01 21:30:21 +0700

    5-Boolean

--- bin/1-Program_HelloWorld.dart
+++ bin/1-Program_HelloWorld.dart
@@ -2,5 +2,5 @@ void main() {
   print('Hello World');
   // Print, menampilkan tulisan di Dart
   // print memiliki parameter data string, menggunakan kutip
-  // Untuk menjalankan dart = dart run bin/Program_HelloWorld.dart
+  // Untuk menjalankan dart [dart run bin/Program_HelloWorld.dart]
 }
---
+++ bin/5-Boolean.dart
@@ -0,0 +1,8 @@
+// Boolean adalah tipe data yang hanya memiliki dua nilai, yaitu benar atau salah.
+// Direpresentasikan dengan kata kunci bool.
+// Benar = True.
+// Salah = false.
+void main() {
+  bool finish = false;
+  print(finish);
+}
