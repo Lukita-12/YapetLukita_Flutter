852f8ca1b82730af3dc1fad898cf54c743ab93e5
Lukita-12 <lukitayapet@gmail.com>
2023-10-02 09:19:05 +0700

    7-Dynamic

---
+++ bin/7-Dybamic.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Dynamic adalah variable yang bisa menampung semua jenis tipe data
+
+// Integer
+  dynamic variable = 100; // Deklarasi variable menjadi dynamic
+  print(variable);
+
+// Boolean
+  variable = true;
+  print(variable);
+
+// String
+  variable = 'Yapet';
+  print(variable);
+}
