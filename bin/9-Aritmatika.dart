b8153e7f0297f7c34ffc48c453c40fc505e91cd2
Lukita-12 <lukitayapet@gmail.com>
2023-10-02 11:53:17 +0700

    9-Aritmatika

---
+++ bin/9-Aritmatika.dart
@@ -0,0 +1,10 @@
+void main() {
+  var result1 = 10 + 10; // Tambah
+  var result2 = 100 - 10; // Kurang
+  var result3 = -5; // Negatif
+  var result4 = 5; // Positif
+  var result5 = 5 * 5; // Kali
+  var result6 = 10 / 3; // Bagi(Double/Decimal)
+  var result7 = 10 ~/ 3; // Bagi(Integer/Round)
+  var result8 = 10 % 3; // Mod(Sisa Bagi)
+}
