5d980ed35f3f14be927a00159e6817094f853320
Lukita-12 <lukitayapet@gmail.com>
2023-10-03 10:55:32 +0700

    20-Null

---
+++ bin/20_Null.dart
@@ -0,0 +1,13 @@
+void main() {
+// Kata kunci null merupakan tipe data representasi data kosong
+// Secara default, variable harus diisi, jika tidak, maka variable tidak bisa digunakan
+/* Membuat variable yang diisi dengan data null, tambahkan ? (tanda tanya), seperti :
+  TipeData? namaVariable  = null;
+*/
+
+  // int number1; // Error!
+  int? number2;
+
+  // print(number1); // Error!
+  print(number2);
+}
