a0bae85db4dfc1ac0ba1e4d66301de3061a50437
Lukita-12 <lukitayapet@gmail.com>
2023-10-03 10:26:37 +0700

    17-Symbol

---
+++ bin/17.Symbol.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Symbol merupakan tipe data yang sebenarnya jarang sekali digunakan.
+  // Symbol sebenarnya bisa digunakan sebagai constant.
+  // Membuat Symbol, gunakan tanda #, atau Symbol(“text”) membuat Symbol dengan nama yang mengandung spasi.
+  Symbol symbol1 = Symbol("Eko Kurniawan"); // Symbol dengan spasi
+  var symbol2 = #belajar; // Symbol tanpa spasi
+
+  print(symbol1);
+  print(symbol2);
+}
