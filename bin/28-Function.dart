b797a9852c0ae66f25876bfe922984db7feb9306
Lukita-12 <lukitayapet@gmail.com>
2023-10-04 15:23:59 +0700

    28.Function

---
+++ bin/28-Function.dart
@@ -0,0 +1,15 @@
+void sayHello() {
+  print('Hello World!');
+}
+
+void main() {
+  sayHello();
+  sayHello();
+}
+
+/*
+Function adalah blok kode program yang akan berjalan saat kita panggil.
+Gunakan kata kunci void, diikuti dengan nama function, kurung () dan diakhiri dengan block.
+Memanggil function, gunakan nama function lalu diikuti dengan kurung ().
+Di bahasa pemrograman lain, Function juga disebut dengan Method.
+*/
