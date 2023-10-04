8dc917e7417e9350130d5c6d5b715cc54685a25f
Lukita-12 <lukitayapet@gmail.com>
2023-10-04 11:30:32 +0700

    24.While loop

---
+++ bin/24-While_Loop.dart
@@ -0,0 +1,11 @@
+void main() {
+  //  Versi perulangan yang lebih sederhana dibanding for loop.
+  //  Hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement.
+
+  var counter = 1;
+
+  while (counter <= 10) {
+    print('Perulangan ke-$counter');
+    counter++;
+  }
+}
