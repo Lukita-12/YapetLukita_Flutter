964c9f1a0c538b35fd5c4b89e194a2dc09e05035
Lukita-12 <lukitayapet@gmail.com>
2023-10-05 13:01:55 +0700

    33.Function short value

---
+++ bin/33-Function_short_exprssion.dart
@@ -0,0 +1,14 @@
+int sum(int first, int second) => first + second;
+
+void main() {
+  print(sum(10, 10));
+  print(sum(20, 20));
+}
+
+/*
+  Dart mendukung function short expression.
+  Dimana jika terdapat sebuah function yang hanya satu baris,
+  kita bisa menyingkatnya secara sederhana.
+  Untuk membuat  function short expression,
+  kita tidak butuh kurung {} dan juga tidak butuh kata kunci return.
+*/
