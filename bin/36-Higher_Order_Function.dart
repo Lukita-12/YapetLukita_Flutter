1b2867a581fb5dd53c95a8dfdfc37c8ac8a8aafe
Lukita-12 <lukitayapet@gmail.com>
2023-10-05 13:54:20 +0700

    37.Anonymous Function

--- bin/36-Higher_Order_Function.dart
+++ bin/36-Higher_Order_Function.dart
@@ -1,19 +1,28 @@
 // Function as Parameter
 void sayHello(String name, String Function(String) filter) {
   var filteredNamed = filter(name);
-  print('Hi $filteredNamed');
+  print('You are $filteredNamed');
 }
 
 // Higher ordeer function
 String filterBadWord(String name01) {
   if (name01 == 'shit') {
-    return '****';
+    return 'Sh*t';
   } else {
     return name01;
   }
 }
 
 void main() {
-  sayHello('Yapet', filterBadWord);
+  sayHello('Good', filterBadWord);
   sayHello('shit', filterBadWord);
 }
+
+/*
+    Higher-Order Function adalah function yang menggunakan function sebagai variable,
+  parameter atau return value.
+    Penggunaan Higher-Order Function kadang berguna ketika kita ingin membuat function
+  yang general dan ingin mendapatkan input yang flexible beruba function, yang bisa
+  dideklarasikan oleh pengguna ketika memanggil function tersebut.
+
+*/
---
+++ bin/37-Anonymous_Function.dart
@@ -0,0 +1,33 @@
+// Anonymous Function as Variable
+var upperFunction = (String name) {
+  return name.toUpperCase();
+};
+
+var lowerFunction = (String name) => name.toLowerCase();
+
+print(upperFunction('Yapet'));
+print(lowerFunction('Lukita'));
+
+
+// Anonymous Function as Parameter
+void sayHello(String name, String Function(String) filter) {
+  var filteredName = filter(name);
+  print('Hi $filteredName');
+}
+
+void main() {
+  sayHello('Yapet Lukita', (name) {
+    return name.toUpperCase();
+  });
+  sayHello('Yapet Lukita', (String name) => name.toLowerCase());
+}
+
+/*
+  Kebanyakan function biasanya memiliki nama, seperti sayHello(), print() dan  lain-lain.
+  Kita juga bisa membuat function yang tidak memiliki nama, atau disebut anonymous
+function. Di bahasa pemrograman lain ada yang memanggilnya lambda.
+  Pembuatan anonymous function mirip seperti function bisanya, namun yang membedakan
+adalah tidak ada nama function nya.
+  Biasanya anonymous function sering digunakan ketika memanggil function yang membutuhkan
+parameter berupa function
+*/
