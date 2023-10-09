b6f306b07f44c587550f55c5fab51ec7fe3b4520
Lukita-12 <lukitayapet@gmail.com>
2023-10-09 13:00:19 +0700

    12.This Keyword

--- bin/Dart OPP/11.variableShadowing.dart
+++ bin/Dart OPP/11.variableShadowing.dart
@@ -1,4 +1,21 @@
+class Person {
+  String name = "Guest"; // Wajib diisi.
+  String? address; // Tidak wajib diisi(nullable).
+  final String country = "Indonesia"; // Tidak bisa diubah.
 
+// Variable shadowing
+  Person(String name, String address) {
+    name = name; // field name tidak berubah
+    address = address; // field address tidak berubah
+  }
+}
+
+void main() {
+  var person = Person("Yapet", "Tamiang Layang");
+  print(person.name);
+  print(person.address);
+  // Variable name & address tidak berubah karena terhalang oleh varible shadowing
+}
 
 /* 
 Variable shadowing adalah kejadian ketika kita membuat nama variable dengan nama yang sama di scope
---
+++ bin/Dart OPP/12.thisKeyword.dart
@@ -0,0 +1,35 @@
+class Person {
+  String name = "Guest"; // Wajib diisi.
+  String? address; // Tidak wajib diisi(nullable).
+  final String country = "Indonesia"; // Tidak bisa diubah.
+
+// KEyword pada Variable shadowing
+  Person(String name, String address) {
+    this.name = name; // keyword
+    this.address = address; // keyword
+  }
+}
+
+void main() {
+  var person = Person("Yapet", "Tamiang Layang");
+  print(person.name);
+  print(person.address);
+  // Sekarang walaupun variable nya sama tapi name & address tetap bisa diakses,
+  // karena Variable shadowing diberikan keyword untuk membedakannya.
+}
+Variable shadowing adalah kejadian ketika kita membuat nama variable dengan nama yang sama di scope
+yang menutupi variable dengan nama yang sama di scope diatasnya.
+Ini biasa terjadi seperti kita membuat nama parameter di method sama dengan nama field di class.
+Saat terjadi variable shadowing, maka secara otomatis variable di scope diatasnya tidak bisa diakses.
+*/
+
+/*
+  Saat kita membuat kode di dalam block constructor atau method di dalam class,
+kita bisa menggunakan kata kunci "this" untuk mengakses object saat ini.
+  Misal kadang kita butuh mengakses sebuah field yang namanya sama dengan parameter
+method, hal ini tidak bisa dilakukan jika langsung menyebut nama field, kita bisa
+mengakses nama field tersebut dengan kata kunci "this".
+  "This" juga tidak hanya digunakan untuk mengakses field milik object saat ini,
+namun juga bisa digunakan untuk mengakses method.
+"This" bisa digunakan untuk mengatasi masalah variable shadowing.
+*/
