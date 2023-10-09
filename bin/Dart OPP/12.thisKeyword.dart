e0db313f88a76283afd24b6cb5fcac4a19b4b30a
Lukita-12 <lukitayapet@gmail.com>
2023-10-09 13:01:25 +0700

    12.This Keyword

--- bin/Dart OPP/12.thisKeyword.dart
+++ bin/Dart OPP/12.thisKeyword.dart
@@ -17,11 +17,6 @@ void main() {
   // Sekarang walaupun variable nya sama tapi name & address tetap bisa diakses,
   // karena Variable shadowing diberikan keyword untuk membedakannya.
 }
-Variable shadowing adalah kejadian ketika kita membuat nama variable dengan nama yang sama di scope
-yang menutupi variable dengan nama yang sama di scope diatasnya.
-Ini biasa terjadi seperti kita membuat nama parameter di method sama dengan nama field di class.
-Saat terjadi variable shadowing, maka secara otomatis variable di scope diatasnya tidak bisa diakses.
-*/
 
 /*
   Saat kita membuat kode di dalam block constructor atau method di dalam class,
