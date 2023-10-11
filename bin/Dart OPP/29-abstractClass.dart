f97ce4ddaf8f3d451b51b8e4be14751dcdb61f07
Lukita-12 <lukitayapet@gmail.com>
2023-10-11 10:32:58 +0700

    29. Abstract class

---
+++ bin/Dart OPP/29-abstractClass.dart
@@ -0,0 +1,14 @@
+import 'data/29-location.dart';
+
+void main() {
+  var city = City('Tamiang Layang');
+
+  // var location = Location(); // error
+}
+
+/*
+Saat kita membuat class, kita bisa menjadikan sebuah class sebagai abstract class.
+Abstract class artinya, class tersebut tidak bisa dibuat sebagai object secara langsung, hanya bisa diturunkan.
+Untuk membuat sebuah class menjadi abstract, kita bisa menggunakan kata kunci abstract sebelum kata kunci class.
+Dengan demikian abstract class bisa kita gunakan sebagai kontrak untuk class child.
+*/
