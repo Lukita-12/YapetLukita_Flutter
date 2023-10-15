500a90651d48ea71c42c8fec79c8276a531796d5
Lukita-12 <lukitayapet@gmail.com>
2023-10-15 16:59:54 +0700

    40. Callable class

---
+++ bin/Dart OPP/31-40/40-callableClass.dart
@@ -0,0 +1,6 @@
+/*
+Callable Class merupakan class yang bisa dipanggil seperti function.
+Untuk membuat Callable Class, kita perlu menambahkan sebuah method bernama call() di class tersebut.
+Parameter dan Return Value dari Method tersebut bisa disesuaikan sesuai keinginan kita.
+Setelah membuat objectnya, kita bisa langsung memanggil method call() tersebut menggunakan nama objectnya saja.
+*/
