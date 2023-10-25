90ee656855075042b27d666a89a14156aa69b466
Lukita-12 <lukitayapet@gmail.com>
2023-10-25 15:10:09 +0700

    02. Gebeneric Function

---
+++ bin/Dart Generic/02-genericFunction.dart
@@ -0,0 +1,9 @@
+import 'data/02-array_helper.dart';
+
+void main() {
+  var number = [1, 2, 3, 4, 5, 6];
+  var names = ["Yapet", "Lukita", "Warm"];
+
+  print(ArrayHelper.count(number));
+  print(ArrayHelper.count(names));
+}
